9a3ae0dc4a180d24915fe28be8d2a6f0da880e8b
Li Xiaoyue <lixiaoyue.314@gmail.com>
2016-04-12 12:27:06 +0800

    update post_travel

--- DatastructureAndAlgorithm/DataStructure-ZhejiangU(20160229-20160603)/week3_TreeTraversalsAgain.py
+++ DatastructureAndAlgorithm/DataStructure-ZhejiangU(20160229-20160603)/week3_TreeTraversalsAgain.py
@@ -1,37 +1,53 @@
-import Stack
-"""
-答案有问题，待解决
-"""
+def print_sequ(sequence):
+    result = ""
+    for item in sequence:
+        result += str(item) + " "
 
-def postorderTraveral():
-    stack = Stack()
-    rt_stack = Stack()
-    lst = ""
-    total_node = int(input())
-    for idx in range(total_node*2):
-        temp = input().split()
-        if temp[0] == "Push":
-            stack.push(int(temp[1]))
-            last = "Push"
-        elif temp[0] == "Pop":
-            if last == "Push":
-                lst += str(stack.pop()) + " "
-            if last == "Pop" and not rt_stack.length():
-                rt_stack.push(stack.pop())
-            elif last == "Pop":
-                lst += str(rt_stack.pop()) + " "
-            last = "Pop"
-        else:
-            print "Invalid input."
+    print(result[:-1])
 
-    while rt_stack.length():
-        lst += str(rt_stack.pop()) + " "
-    while stack.length():
-        lst += str(stack.pop()) + " "
-    return lst[:-1]
+def post_travel(pre, inorder, length):
+    post = []
+    if length:
+        root = pre[0]
+        for idx in range(length):
+            if inorder[idx] == root:
+                break
+        l_length = idx
+        r_length = length - 1 - l_length
+        l_in = inorder[:l_length]
+        r_in = inorder[l_length + 1:]
+        l_pre = pre[1: 1 + l_length]
+        r_pre = pre[l_length + 1:]
+
+        new_l = post_travel(l_pre, l_in, l_length)
+        new_r = post_travel(r_pre, r_in, r_length)
+
+        post.extend(new_l)
+        post.extend(new_r)
+        post.append(root)
+
+    return post
 
 
-tree = postorderTraveral()
-print tree
 
+def main():
+    length = int(input())
+    pre = []
+    pre_temp = []
+    inorder = []
+    post = []
+
+    for dummy in range(length * 2):
+        info = input().split()
+        if info[0] == "Push":
+            pre.append(int(info[1]))
+            pre_temp.append(int(info[1]))
+        elif info[0] == "Pop":
+            inorder.append(pre_temp.pop(-1))
+
+    post = post_travel(pre, inorder, length)
+
+    print_sequ(post)
+    
+main()
 
